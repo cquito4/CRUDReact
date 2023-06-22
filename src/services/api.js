4761c527a9d15f983576ed074156f0f26da128b4
cquito4 <christian.quitoa@ucuenca.edu.ec>
2023-06-22 01:56:44 -0500

    produccion

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000'; // Reemplaza con la URL base de tu API
+const API_BASE_URL = 'https://crudfastapi.onrender.com'; // Reemplaza con la URL base de tu API
 
 export const getUsers = () => {
   return axios.get(`${API_BASE_URL}/user`);
